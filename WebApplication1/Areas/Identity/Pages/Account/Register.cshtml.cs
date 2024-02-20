105c0a0c620057dcd41853408fc0bf4f4ce8a23d
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2024-02-20 15:23:17 +0100

    ujol

--- WebApplication1/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ WebApplication1/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -76,9 +76,21 @@ namespace WebApplication1.Areas.Identity.Pages.Account
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
             [Required]
+            public string FirstName { get; set; }
+            
+            [Required]
+            public string LastName { get; set; } 
+
+            [Required]
             [EmailAddress]
             [Display(Name = "Email")]
             public string Email { get; set; }
+            
+            [Required]
+            public string PhoneNumber { get; set; }
+            
+            [Required]
+            public string Address { get; set; }
 
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
