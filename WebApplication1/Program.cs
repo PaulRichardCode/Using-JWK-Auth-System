fbd637a5433e6c23bbb1d5c89007492329bf47b1
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2024-02-20 14:55:26 +0100

    pA

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -15,7 +15,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString);
 });
 
-builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false).AddEntityFrameworkStores<ApplicationDbContext>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
