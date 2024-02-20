9bc51c241ef04aa075c43bcca160732063caf64d
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2024-02-20 15:55:24 +0100

    God of messs

--- WebApplication1/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ WebApplication1/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -125,7 +125,14 @@ namespace WebApplication1.Areas.Identity.Pages.Account
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
-                var user = CreateUser();
+                var user = new ApplicationUser()
+                {
+                FirstName = Input.FirstName,
+                LastName = Input.LastName,
+                Email = Input.Email,
+                PhoneNumber = Input.PhoneNumber,
+                Address = Input.Address
+                };
 
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
